8fb067a8a683511651c38be47f115433c84a4e48
elgene <eedren@gmail.com>
2023-02-10 22:44:39 +0800

    fix:send email request handling

--- pages/api/send-email/index.ts
+++ pages/api/send-email/index.ts
@@ -3,29 +3,26 @@ import Email from "../../../emails";
 import { render } from "@react-email/render";
 import sendgrid from "@sendgrid/mail";
 
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY ?? '');
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY ?? "");
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   if (req.method === "POST") {
-    console.log(req.body);
-    const emailHtml = render(Email());
-    const options = {
-      from: "eedren@gmail.com",
-      to: "zolous.official@gmail.com",
-      subject: "Download Your Results Now!",
-      html: emailHtml,
-    };
-    sendgrid
-      .send(options)
-      .then(() => {
-        console.log("Email sent");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    const data = JSON.parse(req.body);
+
+    if (data.email !== null) {
+      const emailHtml = render(Email());
+      const options = {
+        from: "eedren@gmail.com",
+        to: data.email,
+        subject: "Download Your Results Now!",
+        html: emailHtml,
+      };
+
+      sendgrid.send(options);
+    }
 
     res.status(200).json({ message: "Success" });
   } else {
