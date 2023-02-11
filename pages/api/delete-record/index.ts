6b966be40cc5d49cfd178aa90712d0a3bd15e890
elgene <eedren@gmail.com>
2023-02-11 23:24:46 +0800

    fix:remove method for delete-record

--- pages/api/delete-record/index.ts
+++ pages/api/delete-record/index.ts
@@ -5,43 +5,39 @@ import { verifySignature } from "@upstash/qstash/nextjs";
 const prisma = new PrismaClient();
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "GET") {
-    const submissions = await prisma.submission.findMany();
-    const currDate = new Date().getTime();
-    let submissionID: string[] = [];
-    let imageID: string[] = [];
-    submissions.forEach((submission) => {
-      if (Math.abs(currDate - submission.createdAt.getTime()) / 36e5 > 24) {
-        submissionID.push(submission.id);
-        imageID.push(submission.img_public_id);
-      }
-    });
+  const submissions = await prisma.submission.findMany();
+  const currDate = new Date().getTime();
+  let submissionID: string[] = [];
+  let imageID: string[] = [];
+  submissions.forEach((submission) => {
+    if (Math.abs(currDate - submission.createdAt.getTime()) / 36e5 > 24) {
+      submissionID.push(submission.id);
+      imageID.push(submission.img_public_id);
+    }
+  });
 
-    //delete images from cloudinary
-    await fetch(
-      process.env.NODE_ENV === "production"
-        ? "https://brighten.ai/api/delete-image"
-        : "http://localhost:3000/api/delete-image",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(imageID),
+  //delete images from cloudinary
+  await fetch(
+    process.env.NODE_ENV === "production"
+      ? "https://brighten.ai/api/delete-image"
+      : "http://localhost:3000/api/delete-image",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    );
+      body: JSON.stringify(imageID),
+    },
+  );
 
-    const updatedSubmissions = await prisma.submission.deleteMany({
-      where: {
-        id: {
-          in: submissionID,
-        },
+  const updatedSubmissions = await prisma.submission.deleteMany({
+    where: {
+      id: {
+        in: submissionID,
       },
-    });
-    res.status(200).json(updatedSubmissions);
-  } else {
-    res.status(500).json({ message: "Method not allowed" });
-  }
+    },
+  });
+  res.status(200).json(updatedSubmissions);
 }
 
 export default verifySignature(handler);
