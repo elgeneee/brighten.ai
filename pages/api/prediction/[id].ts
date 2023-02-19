c64b5cc5b51ef834fff3ba256bcc7e644063b6c0
elgene <eedren@gmail.com>
2023-02-19 17:39:16 +0800

    fix:remove quote from authorization header

--- pages/api/prediction/[id].ts
+++ pages/api/prediction/[id].ts
@@ -9,7 +9,7 @@ export default async function handler(
     "https://api.replicate.com/v1/predictions/" + req.query.id,
     {
       headers: {
-        "Authorization": `Token ${process.env.REPLICATE_API_TOKEN}`,
+        Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
         "Content-Type": "application/json",
       },
     },
